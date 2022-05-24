67af7e485bce9a4aa6a2fcf76a84af248d54e767
unknown <SmithaRai.com>
2022-05-24 14:32:37 +0530

    Added : added validations for salary and name

--- src/main/java/com/bridgelabz/employeepayrollapps/Controller/EmployeePayrollAppsController.java
+++ src/main/java/com/bridgelabz/employeepayrollapps/Controller/EmployeePayrollAppsController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -35,7 +36,7 @@ public class EmployeePayrollAppsController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDTO> addEmployeePayrollData(@RequestBody EmployeePayrollDTO empPayrollDTO) {
+    public ResponseEntity<ResponseDTO> createEmployeePayrollData(@Valid@RequestBody EmployeePayrollDTO empPayrollDTO) {
         EmployeePayrollData empData = null;
         empData = employeePayrollService.createEmployeePayrollData(empPayrollDTO);
         ResponseDTO respDTO = new ResponseDTO("Created Employee Payroll Data Successful",empData);
@@ -45,7 +46,7 @@ public class EmployeePayrollAppsController {
 
 
     @PutMapping("/update/{empId}")
-    public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@PathVariable("empId")int empId,@RequestBody EmployeePayrollDTO empPayrollDTO){
+    public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@PathVariable("empId")int empId,@Valid @RequestBody EmployeePayrollDTO empPayrollDTO){
         EmployeePayrollData empData = null ;
         empData = employeePayrollService.updateEmployeePayrollData(empId,empPayrollDTO);
         ResponseDTO respDTO = new ResponseDTO("Updated Employee Payroll Data Successfully",empData);
--- src/main/java/com/bridgelabz/employeepayrollapps/DTO/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapps/DTO/EmployeePayrollDTO.java
@@ -1,7 +1,14 @@
 package com.bridgelabz.employeepayrollapps.DTO;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 public class EmployeePayrollDTO {
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s] {2,} $" , message = "Employee name Invalid")
+    @NotEmpty(message = "Employee name cannot be null")
     public String name;
+    @Min(value = 500,message = "Min Wage should be more than 500")
     public long salary;
 
     public EmployeePayrollDTO(String name , long salary ) {
